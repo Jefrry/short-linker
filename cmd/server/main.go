7fe66a49233eb85550fa1671ba997efe3214005f
Egor Ilin <skeetch2010@gmail.com>
2025-12-04 14:17:25 +0400

    main refactoring

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,104 +1,30 @@
 package main
 
 import (
-	"database/sql"
 	"log"
 	"net/http"
-	"github.com/golang-migrate/migrate/v4"
-	_ "github.com/golang-migrate/migrate/v4/database/postgres"
-	_ "github.com/golang-migrate/migrate/v4/source/file"
-	_ "github.com/jackc/pgx/v5/stdlib"
 	"go.uber.org/zap"
 
+	"short-linker/internal/app"
 	"short-linker/internal/config"
-	"short-linker/internal/handler"
-	"short-linker/internal/repository"
-	"short-linker/internal/router"
-	"short-linker/internal/service"
-	"short-linker/internal/storage"
 )
 
 func main() {
 	cfg := config.GetConfig()
 
-	logger, err := initLoger()
+	a, err := app.NewApp(cfg)
 	if err != nil {
-		log.Fatalf("failed to initialize logger: %v", err)
+		log.Fatalf("failed to init app: %v", err)
 	}
 
-	var db *sql.DB
-	if cfg.DatabaseDsn != "" {
-		db, err = initDB(cfg.DatabaseDsn)
-		if err != nil {
-			logger.Fatal("failed to initialize database", zap.Error(err))
+	defer func() {
+		if a.DB != nil {
+			_ = a.DB.Close()
 		}
-		defer db.Close()
+		_ = a.Logger.Sync()
+	}()
 
-		err = initMigration(cfg.DatabaseDsn)
-		if err != nil {
-			logger.Fatal("failed to run database migrations", zap.Error(err))
-		}
+	if err := http.ListenAndServe(cfg.Address, a.Router); err != nil {
+		a.Logger.Fatal("server error", zap.Error(err))
 	}
-
-	storage := storage.NewMemory()
-
-	tokenService := service.NewTokenService(cfg.JWTSecret)
-
-	pingHandler := handler.NewPingHandler(db)
-
-	// TODO: Add context to handlers
-	linkRepo := repository.NewLinkRepository(storage, db)
-	linkService := service.NewLinkService(linkRepo, cfg.BaseShortURL) // Do I need to pass BaseShortURL here or in repo?
-	linkHandler := handler.NewLinkHandler(linkService)
-
-	userRepo := repository.NewUserRepository(db)
-	userService := service.NewUserService(userRepo, tokenService)
-	userHandler := handler.NewUserHandler(userService)
-
-	r := router.NewRouter(cfg.JWTSecret, pingHandler, linkHandler, userHandler).SetupRoutes(logger)
-
-	err = http.ListenAndServe(cfg.Address, r)
-	if err != nil {
-		panic(err)
-	}
-}
-
-// Where to collect all init functions?
-func initLoger() (*zap.Logger, error) {
-	logger, err := zap.NewProduction()
-	if err != nil {
-		panic(err) // Do I need panic with no logger?
-	}
-	defer logger.Sync()
-	return logger, err
-}
-
-func initDB(dsn string) (*sql.DB, error) {
-	db, err := sql.Open("pgx", dsn)
-	if err != nil {
-		return nil, err
-	}
-
-	err = db.Ping()
-	if err != nil {
-		return nil, err
-	}
-
-	return db, nil
-}
-
-func initMigration(dsn string) error {
-	m, err := migrate.New(
-		"file://migrations",
-		dsn)
-	if err != nil {
-		return err
-	}
-
-	err = m.Up()
-	if err != nil && err != migrate.ErrNoChange {
-		return err
-	}
-
-	return nil
 }
---
+++ internal/app/init.go
@@ -0,0 +1,103 @@
+package app
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"go.uber.org/zap"
+
+	"short-linker/internal/config"
+	"short-linker/internal/handler"
+	"short-linker/internal/repository"
+	"short-linker/internal/router"
+	"short-linker/internal/service"
+	"short-linker/internal/storage"
+)
+
+type App struct {
+	Cfg    *config.Config
+	Logger *zap.Logger
+	DB     *sql.DB
+	Router http.Handler
+}
+
+func NewApp(cfg *config.Config) (*App, error) {
+	logger, err := initLogger()
+	if err != nil {
+		return nil, fmt.Errorf("init logger: %w", err)
+	}
+
+	var db *sql.DB
+	if cfg.DatabaseDsn != "" {
+		db, err = initDB(cfg.DatabaseDsn)
+		if err != nil {
+			logger.Error("failed to initialize database", zap.Error(err))
+			return nil, fmt.Errorf("init db: %w", err)
+		}
+
+		if err = initMigration(cfg.DatabaseDsn); err != nil {
+			logger.Error("failed to run database migrations", zap.Error(err))
+			return nil, fmt.Errorf("init migrations: %w", err)
+		}
+	}
+
+	memStorage := storage.NewMemory()
+
+	linkRepo := repository.NewLinkRepository(memStorage, db)
+	userRepo := repository.NewUserRepository(db)
+
+	linkService := service.NewLinkService(linkRepo, cfg.BaseShortURL)
+	userService := service.NewUserService(userRepo, service.NewTokenService(cfg.JWTSecret))
+
+	pingHandler := handler.NewPingHandler(db)
+	linkHandler := handler.NewLinkHandler(linkService)
+	userHandler := handler.NewUserHandler(userService)
+
+	r := router.NewRouter(cfg.JWTSecret, pingHandler, linkHandler, userHandler).SetupRoutes(logger)
+
+	return &App{
+		Cfg:    cfg,
+		Logger: logger,
+		DB:     db,
+		Router: r,
+	}, nil
+}
+
+
+func initLogger() (*zap.Logger, error) {
+	return zap.NewProduction()
+}
+
+func initDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func initMigration(dsn string) error {
+	m, err := migrate.New(
+		"file://migrations",
+		dsn,
+	)
+	if err != nil {
+		return err
+	}
+
+	if err = m.Up(); err != nil && err != migrate.ErrNoChange {
+		return err
+	}
+
+	return nil
+}
